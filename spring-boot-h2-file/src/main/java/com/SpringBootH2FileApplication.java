b892f310f28853ad30b535cf2f9080c6dc9fcc8a
Minwoo-Kim <bluesky@hatiolab.com>
2015-12-12 17:27:02 +0900

    작업중

--- spring-boot-h2-file/src/main/java/com/SpringBootH2FileApplication.java
+++ spring-boot-h2-file/src/main/java/com/SpringBootH2FileApplication.java
@@ -2,11 +2,19 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootH2FileApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootH2FileApplication.class, args);
-    }
+	@RequestMapping("/**/*")
+	public String index() {
+		return "helloworld!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2FileApplication.class, args);
+	}
 }
